dbb8037ce7a18e22ea8cf98c8cbb76d9a5d29f0a
Resa Suhendra <resasuhandra07@gmail.com>
2021-11-04 13:29:48 +0700

    udah fix input message

--- lib/ui/chat_page.dart
+++ lib/ui/chat_page.dart
@@ -39,64 +39,24 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
+  onTapSend() {
+    MessageModel mess = MessageModel(
+      dateTime: DateTime.now(),
+      type: 'text',
+      uidSender: sender!.uid,
+      uidReceiver: receiver.uid,
+      message: _messageController.text,
+    );
+    MessageService.sendMessage(mess, sender!, receiver).then((value) {
+      scroll.jumpTo(scroll.position.maxScrollExtent);
+    });
+    _messageController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     AppBar appbar = AppBar(
       title: Text(receiver.name ?? "No Name"),
-    );
-
-    Widget messageField = Flexible(
-      child: Container(
-        color: Colors.green.withOpacity(0.2),
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minWidth: MediaQuery.of(context).size.width,
-            maxWidth: MediaQuery.of(context).size.width,
-            minHeight: 25.0,
-            maxHeight: 135.0,
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  child: Scrollbar(
-                    child: TextField(
-                      cursorColor: Colors.red,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      controller: _messageController,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(
-                            top: 2.0, left: 13.0, right: 13.0, bottom: 2.0),
-                        hintText: "Type your message",
-                        hintStyle: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              IconButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    MessageModel mess = MessageModel(
-                      dateTime: DateTime.now(),
-                      type: 'text',
-                      uidSender: sender!.uid,
-                      uidReceiver: receiver.uid,
-                      message: _messageController.text,
-                    );
-                    MessageService.sendMessage(mess, sender!, receiver);
-                    _messageController.clear();
-                    scroll.jumpTo(scroll.position.maxScrollExtent);
-                  },
-                  icon: const Icon(Icons.send))
-            ],
-          ),
-        ),
-      ),
     );
 
     Widget chats = StreamBuilder(
@@ -110,33 +70,94 @@ class _ChatPageState extends State<ChatPage> {
           if (snapshot.data!.size < 1) {
             return const Center(child: Text("Belum ada pesan"));
           } else {
-            return ListView(
-              controller: scroll,
-              children: snapshot.data!.docs
-                  .map((doc) => BubbleNormal(
-                      text: doc['message'],
-                      color: doc['uidSender'].toString().contains("${sender!.uid}")
-                          ? Colors.green
-                          : Colors.white,
-                      tail: false,
-                      isSender: doc['uidSender'].toString().contains("${sender!.uid}")
-                          ? true
-                          : false))
-                  .toList(),
+            return Scrollbar(
+              child: ListView(
+                controller: scroll,
+                physics: const BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics()),
+                children: snapshot.data!.docs
+                    .map((doc) => BubbleNormal(
+                        text: doc['message'],
+                        color: doc['uidSender']
+                                .toString()
+                                .contains("${sender!.uid}")
+                            ? Colors.green
+                            : Colors.white,
+                        tail: false,
+                        isSender: doc['uidSender']
+                                .toString()
+                                .contains("${sender!.uid}")
+                            ? true
+                            : false))
+                    .toList(),
+              ),
             );
           }
         }
       },
     );
 
+
+    Widget inputMessage = Container(
+      color: Colors.grey,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Flexible(
+            child: Container(
+              margin: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: MediaQuery.of(context).size.width,
+                  maxWidth: MediaQuery.of(context).size.width,
+                  minHeight: 20.0,
+                  maxHeight: 135.0,
+                ),
+                child: Scrollbar(
+                  child: TextField(
+                    cursorColor: Colors.red,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    controller: _messageController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(
+                          top: 2.0, left: 13.0, right: 13.0, bottom: 2.0),
+                      hintText: "Type your message",
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: GestureDetector(
+              onTap: onTapSend,
+              child: const CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 20,
+                child: Center(child: Icon(Icons.send_rounded)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: appbar,
       body: Container(
-        padding: EdgeInsets.only(bottom: 50),
+        padding: const EdgeInsets.only(bottom: 60),
         child: chats,
       ),
-      bottomSheet: messageField,
+      bottomSheet: inputMessage,
     );
   }
 }
